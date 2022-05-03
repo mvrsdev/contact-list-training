6fa815aa2feff85289c1182a962416ba8bce746e
mvrsdev <93167052+mvrsdev@users.noreply.github.com>
2022-05-03 16:29:16 -0300

    Create new contact function

--- scripts/contactList.js
+++ scripts/contactList.js
@@ -1,28 +1,36 @@
-const contactData = [ 
+const contactData = [
   {
-  name: 'Marcus',
-  surname: 'Santos',
-  email: 'myemail',
-  server: '@server.com',
-  cellNumber: 11986628317,
-},
-{
-  name: 'Thiago',
-  surname: 'Dias',
-  email: 'anothermail',
-  server: '@server.com',
-  cellNumber: 11989890909,
-}
-]
+    name: 'Marcus',
+    surname: 'Santos',
+    email: 'myemail',
+    server: '@server.com',
+    cellNumber: 11986628317,
+  },
+  {
+    name: 'Thiago',
+    surname: 'Dias',
+    email: 'anothermail',
+    server: '@server.com',
+    cellNumber: 11989890909,
+  },
+];
 
 function getContactFullName(contactData) {
   return `${contactData.name} ${contactData.surname}`;
 }
-const contactFullName = getContactFullName (contactData[1]);
+const contactFullName = getContactFullName(contactData[1]);
 
 function getFullEmail(contactData) {
   return `${contactData.email}${contactData.server}`;
 }
-const fullEmail = getFullEmail (contactData[1])
+const fullEmail = getFullEmail(contactData[1]);
 
-console.log(fullEmail);
+function addNewContact(name, surname, email, server, cellNumber) {
+  return (
+    contactData.push('name'),
+    contactData.push('surname'),
+    contactData.push('email'),
+    contactData.push('server'),
+    contactData.push('cellNumber')
+  );
+}
