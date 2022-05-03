bc9e8873b2a1183f2b64d1599b06038f97a4b0fe
mvrsdev <93167052+mvrsdev@users.noreply.github.com>
2022-05-03 11:17:12 -0300

    Create contact full name function

--- contactList.js
+++ contactList.js
@@ -4,3 +4,9 @@ const contactData = {
   email: 'myemail@server.com',
   cellNumber: 11986628317
 };
+
+function getContactFullName (contactData) {
+  return `${contactData.name} ${contactData.surname}`;
+}
+const contactFullName = getContactFullName (contactData);
+
